6d4ecfa03d6cb4c43633706a9ed9a44fc330dd51
icokie <yu.troynov@zaycev.net>
2017-09-27 14:04:37 +0500

    linted + port changed

--- config/dev.js
+++ config/dev.js
@@ -1,14 +1,14 @@
 module.exports = {
-    
-    backendPort: 3030,
-    
+
+    backendPort: 8080,
+
     apiProxy: {
-        host: '',
+        host: ''
     },
-    
+
     cache: {
         back: {cache: true, expires: 30000},
         front: {cache: true, expires: 30000}
     }
-    
+
 };
