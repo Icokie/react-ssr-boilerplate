eefa19ffeb8227fb24681799729d06d5e6376b24
icokie <yu.troynov@zaycev.net>
2017-09-27 12:21:52 +0500

    config added

---
+++ config/dev.js
@@ -0,0 +1,14 @@
+module.exports = {
+    
+    backendPort: 3030,
+    
+    apiProxy: {
+        host: '',
+    },
+    
+    cache: {
+        back: {cache: true, expires: 30000},
+        front: {cache: true, expires: 30000}
+    }
+    
+};
